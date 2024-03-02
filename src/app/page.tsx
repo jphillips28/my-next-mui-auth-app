30121674d3eafc028e2d5ee308019f9a8ca34594
Jacob Phillips <jacob.p.phillips.5709@gmail.com>
2024-03-02 17:38:37 -0600

    Tweaks `h1` text-align: center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <main>
       <Container>
-        <Typography variant="h1">
+        <Typography
+          variant="h1"
+          textAlign="center"
+        >
           Hello You!!!
         </Typography>
       </Container>

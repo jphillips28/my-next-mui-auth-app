c53fedfa22a8e6e6c6849cfc20d248379743e738
Jacob Phillips <jacob.p.phillips.5709@gmail.com>
2024-03-03 16:50:01 -0600

    Slims `api/auth` options

--- src/libs/authOptions.ts
+++ src/libs/authOptions.ts
@@ -8,23 +8,5 @@ export const authOptions = {
 		}),
 	],
 	secret: process.env.NEXTAUTH_SECRET,
-	// callbacks: {
-	// 	async jwt({ token, profile }: any) {
-	// 		if (profile) {
-	// 			token.externalId = profile.extension_ExternalID;
-	// 			token.postalCode = profile.postalCode;
-	// 			token.userType = profile.extension_UserType;
-	// 		}
-	// 		return token;
-	// 	},
-	// 	async session({ session, token }: any) {
-	// 		if (token) {
-	// 			session.user.externalId = token.externalId;
-	// 			session.user.postalCode = token.postalCode;
-	// 			session.user.userType = token.userType;
-	// 		}
-	// 		return session;
-	// 	},
-	// },
-	debug: true,
+	// debug: true,
 };
